9173b1ccdb146e9606d72f3279b01d0fe4a48b4a
pmunar <pere.munaradrover@gmail.com>
2017-12-20 10:53:21 +0100

    modified the grib_utils.py script to improve the reading time of grib files. Now it saves 50% of time while reading and selecting the desired parameters

--- gdas_scripts/rdams-client.py
+++ gdas_scripts/rdams-client.py
@@ -60,7 +60,9 @@ def download_file(remfile,outfile):
     frequest = urllib.request.Request(remfile)
     fresponse = urllib.request.urlopen(remfile)
     handle = open(outfile, 'w')
-    handle.write(fresponse.read())
+    s = fresponse.read()
+    print (type(s))
+    handle.write(str(s.decode('utf-8', 'ignore')))
     handle.close()
 
 # get_userinfo() : get username and password
@@ -78,13 +80,14 @@ def add_http_auth(url,user,pasw):
         urllib.request.install_opener(opener)
         return opener
 
+
 # add_http_cookie(url,authstring): Get and add authentication cookie to http file download handler
 def add_http_cookie(url,authstring):
         cj = http.cookiejar.MozillaCookieJar(cookie_file)
-        openrf=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-        frequest = urllib.request.Request(url,authstring)
+        openrf = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+        frequest = urllib.request.Request(url, authstring, headers={'User-Agent': 'Mozilla/5.0'})
         cj.add_cookie_header(frequest)
-        response=openrf.open(frequest)
+        response = openrf.open(frequest)
         openerf = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
         urllib.request.install_opener(openerf)
 
@@ -144,55 +147,55 @@ loginurl='https://rda.ucar.edu/cgi-bin/login'
 exitstring="\nUsage: \nrdams-client.py -get_summary <dsnnn.n>\nrdams-client.py -get_metadata <dsnnn.n>\nrdams-client.py -get_param_summary <dsnnn.n>\nrdams-client.py -submit [control_file_name]\nrdams-client.py -get_status <RequestIndex> <-proc_status>\nrdams-client.py -download [RequestIndex]\nrdams-client.py -globus_download [RequestIndex]\nrdams-client.py -purge [RequestIndex]\nrdams-client.py -get_control_file_template <dsnnn.n>\nrdams-client.py -help\n\n" 
 
 if (len(sys.argv)>1):
-	if(sys.argv[1]=="-get_summary"):
-		print('\nGetting summary information.  Please wait as this may take awhile.\n')
-		theurl = base+'summary' 
-		if(len(sys.argv)>2):
-    			theurl = base+'summary/'+sys.argv[2]
-	elif(sys.argv[1]=="-get_metadata"):
-		print('\nGetting metadata.  Please wait as this may take awhile.\n')
-		theurl = base+'metadata'
-		if(len(sys.argv)==3):
-			theurl = base+'metadata/'+sys.argv[2]
-		elif(len(sys.argv)==4):
-			theurl = base+'metadata/'+sys.argv[2]+'/formatted'
-	elif(sys.argv[1]=="-get_param_summary"):
+    if(sys.argv[1]=="-get_summary"):
+        print('\nGetting summary information.  Please wait as this may take awhile.\n')
+        theurl = base+'summary'
+        if(len(sys.argv)>2):
+                theurl = base+'summary/'+sys.argv[2]
+    elif(sys.argv[1]=="-get_metadata"):
+        print('\nGetting metadata.  Please wait as this may take awhile.\n')
+        theurl = base+'metadata'
+        if(len(sys.argv)==3):
+            theurl = base+'metadata/'+sys.argv[2]
+        elif(len(sys.argv)==4):
+            theurl = base+'metadata/'+sys.argv[2]+'/formatted'
+    elif(sys.argv[1]=="-get_param_summary"):
                 print('\nGetting parameter summary.  Please wait as this may take awhile.\n')
                 theurl = base+'paramsummary'
                 if(len(sys.argv)==3):
                         theurl = base+'paramsummary/'+sys.argv[2]
                 elif(len(sys.argv)==4):
                         theurl = base+'paramsummary/'+sys.argv[2]+'/formatted'
-	elif(sys.argv[1]=="-help"):
+    elif(sys.argv[1]=="-help"):
                 theurl = base+'help'
-	elif(sys.argv[1]=="-get_control_file_template"):
+    elif(sys.argv[1]=="-get_control_file_template"):
                 theurl = base+'template'
                 controlfile='./dsnnn.n_control_file'
                 if(len(sys.argv)>2):
                         theur = base+'template/'+sys.argv[2]
                         controlfile='./'+sys.argv[2]+'_control_file'
-	elif(sys.argv[1]=="-get_status"):
+    elif(sys.argv[1]=="-get_status"):
                 theurl = base+'request'
                 if(len(sys.argv)==3):
                     theurl = base+'request/'+sys.argv[2]
                 elif(len(sys.argv)==4):
                     theurl = base+'request/'+sys.argv[2]+'/'+sys.argv[3]
-	elif(sys.argv[1]=="-download"):
+    elif(sys.argv[1]=="-download"):
                 if(len(sys.argv)>2):
                     theurl = base+'request/'+sys.argv[2]+'/filelist'
                 else:
                     sys.exit("\nUsage: \nrdams-client.py -download [RequestIndex]\n")
-	elif(sys.argv[1]=="-globus_download"):
+    elif(sys.argv[1]=="-globus_download"):
                 if(len(sys.argv)>2):
                     theurl = base+'request/'+sys.argv[2]+'/-globus_download'
                 else:
                     sys.exit("\nUsage: \nrdams-client.py -globus_download [RequestIndex]\n")
-	elif(sys.argv[1]=="-purge"):
+    elif(sys.argv[1]=="-purge"):
                 if(len(sys.argv)>2):
                     theurl = base+'request/'+sys.argv[2]
                 else:
                     sys.exit("\nUsage: \nrdams-client.py -purge [RequestIndex]\n")
-	elif(sys.argv[1]=="-submit"):
+    elif(sys.argv[1]=="-submit"):
                 if(len(sys.argv)>2):
                     theurl = base+'request'
                     with open (sys.argv[2], "r") as myfile:
@@ -201,7 +204,7 @@ if (len(sys.argv)>1):
                                                     continue
                                     li=line.rstrip()
                                     (key,value)=li.split('=',2)
-                                    controlparms[key]=value	
+                                    controlparms[key]=value
                     jsondata='{'
                     for k in list(controlparms.keys()):
                                 jsondata+='"'+k+'"'+":"+'"'+controlparms[k]+'",'
@@ -209,77 +212,81 @@ if (len(sys.argv)>1):
                     jsondata+='}'
                     print('\nSubmitting request.  Please wait as this may take awhile.\n')
                 else:
-                    sys.exit("\nUsage: \nrdams-client.py -submit [control_file_name]\n") 			
-	else:
-		sys.exit(exitstring)
+                    sys.exit("\nUsage: \nrdams-client.py -submit [control_file_name]\n")
+    else:
+        sys.exit(exitstring)
 else:
-	sys.exit(exitstring)
+    sys.exit(exitstring)
 
 
 if os.path.isfile(pwfile) and os.path.getsize(pwfile) >0 :
-	(username,password)=read_pw_file(pwfile)
+    (username,password)=read_pw_file(pwfile)
 else:
-	(username,password)=get_userinfo()
+    (username,password)=get_userinfo()
 
 opener = add_http_auth(theurl,username,password)
 
 if(len(jsondata)>1):
-	f = urllib.parse.urlencode(json.loads(jsondata))
-	f = f.encode('utf-8')
-	request = urllib.request.Request(theurl,f)
-#	request = requests.post(theurl,data=jsondata,headers={'Content-type': 'application/json'})
+    f = urllib.parse.urlencode(json.loads(jsondata))
+    f = f.encode('utf-8')
+    request = urllib.request.Request(theurl,f)
+#    request = requests.post(theurl,data=jsondata,headers={'Content-type': 'application/json'})
 else:
-	request = urllib.request.Request(theurl)
+    request = urllib.request.Request(theurl)
 
 if(sys.argv[1]=="-purge"):
         request.get_method = lambda: 'DELETE'
 
 try:
-	url = opener.open(request)
+    url = opener.open(request)
 except urllib.error.HTTPError as e:
-	if e.code == 401:
-		print('RDA username and password invalid.  Please try again\n')
-		(username,password)=get_userinfo()
-		opener = add_http_auth(theurl,username,password)
-		try:
-			url = opener.open(request)
-		except urllib.error.HTTPError as e:
-			if e.code == 401:
-				print('RDA username and password invalid, or you are not authorized to access this dataset.\n')
-				print('Please verify your login information at http://rda.ucar.edu\n.')
-				sys.exit()
+    if e.code == 401:
+        print('RDA username and password invalid.  Please try again\n')
+        (username,password)=get_userinfo()
+        opener = add_http_auth(theurl,username,password)
+        try:
+            url = opener.open(request)
+        except urllib.error.HTTPError as e:
+            if e.code == 401:
+                print('RDA username and password invalid, or you are not authorized to access this dataset.\n')
+                print('Please verify your login information at http://rda.ucar.edu\n.')
+                sys.exit()
 
 
 write_pw_file(pwfile,username,password)
 
 
 if(sys.argv[1]=="-get_control_file_template"):
- 	print('\nWriting example control file to '+controlfile+'\n')
- 	fo=open(controlfile, "wb")
- 	fo.write(url.read())
- 	fo.close    
- 	sys.exit()
+    print('\nWriting example control file to '+controlfile+'\n')
+    fo=open(controlfile, "wb")
+    fo.write(url.read())
+    fo.close
+    sys.exit()
 if(sys.argv[1]=="-download"):
-	authdata='email='+username+'&password='+password+'&action=login'
+    authdata='email='+username+'&password='+password+'&action=login'
+#    authdata = urllib.parse.urlencode(authdata)
+#    authdata = authdata.encode('utf-8')
 
-	jsonfilelist=url.read()
 
-	if(jsonfilelist[0]!="{"):
-        	print(jsonfilelist)
-        	sys.exit()
+    jsonfilelist=url.read()
+    j_d = jsonfilelist.decode("ascii")
 
-	filelist=json.loads(jsonfilelist)
-	length=len(filelist)
+    if(j_d[0]!="{"):
+            print(j_d)
+            sys.exit()
 
-	directory='rda_request_'+sys.argv[2]
+    filelist=json.loads(j_d)
+    length=len(filelist)
 
-	# get cookie required to download data files
-	add_http_cookie(loginurl,authdata)
+    directory='rda_request_'+sys.argv[2]
 
-	print("\n\nStarting Download.\n\n")
+    # get cookie required to download data files
+    add_http_cookie(loginurl,authdata)
 
-	download_files(filelist,directory)
+    print("\n\nStarting Download.\n\n")
 
-	sys.exit()
+    download_files(filelist,directory)
+
+    sys.exit()
 
 print(url.read())
