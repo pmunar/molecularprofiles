d2f154b26dbf2675b7eb444e271e4fcb04749cbf
pmunar <pere.munaradrover@gmail.com>
2018-01-12 14:05:10 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='molecularprofiles',
+      version='0.1',
+      description='Set of tools to work with grib2 files and plot them',
+      url='http://github.com/pmunar/molecularprofiles',
+      author='Pere Munar-Adrover',
+      author_email='pere.munaradrover@gmail.com',
+      license='None',
+      packages=['molecularprofiles'],
+      zip_safe=False)
