1b9f4752027e4770f621aea0c04d94f90115467c
pmunar <pere.munaradrover@gmail.com>
2018-02-02 14:41:17 +0100

    added the T, U, V and RH variables to the output that the main program will read

--- molecularprofiles/utils/read_txt_gribfile.py
+++ molecularprofiles/utils/read_txt_gribfile.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def read_file(file, epoch_text):
     global months, month
-    print("loading and selecting ecmwf data")
+    print("loading and selecting data")
     file = open(file)
     date, year, month, day, hour, p, T, h, n, U, V, RH = np.loadtxt(file, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                                                                                    11), unpack=True)
@@ -36,12 +36,24 @@ def read_file(file, epoch_text):
                           (month == epoch[3]) | (month == epoch[4])]
         p = p[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
                           (month == epoch[3]) | (month == epoch[4])]
+        T = T[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+                          (month == epoch[3]) | (month == epoch[4])]
+        U = U[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+                          (month == epoch[3]) | (month == epoch[4])]
+        V = V[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+                          (month == epoch[3]) | (month == epoch[4])]
+        RH = RH[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+                          (month == epoch[3]) | (month == epoch[4])]
 
     elif epoch_text == 'summer':
         mjd = mjd[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
         h = h[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
         n = n[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
         p = p[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
+        T = T[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
+        U = U[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
+        V = V[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
+        RH = RH[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])]
 
     elif epoch_text == 'intermediate':
         mjd = mjd[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
@@ -52,13 +64,25 @@ def read_file(file, epoch_text):
                           (month == epoch[3])]
         p = p[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
                           (month == epoch[3])]
+        T = T[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+              (month == epoch[3])]
+        U = U[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+              (month == epoch[3])]
+        V = V[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+              (month == epoch[3])]
+        RH = RH[(month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) |
+              (month == epoch[3])]
 
     elif epoch_text == 'all':
         mjd = mjd
         h = h
         n = n
         p = p
+        T = T
+        U = U
+        V = V
+        RH = RH
 
 
 
-    return mjd, year, month, day, hour, p, h, n
+    return mjd, year, month, day, hour, p, h, n, T, U, V, RH
