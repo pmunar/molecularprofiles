0e05ab0b3c55f1bcdf5273f7d24bd85e3582a6c8
pmunar <pere.munar@uab.cat>
2018-10-17 15:50:19 +0200

    removed functions that are now in observatory.py

--- molecularprofiles/utils/grib_utils.py
+++ molecularprofiles/utils/grib_utils.py
@@ -4,6 +4,7 @@ from builtins import str
 import numpy as np
 import pygrib as pg
 from molecularprofiles.utils.meteorological_constants import *
+from molecularprofiles.utils.observatory import *
 import sys
 import gc
 import multiprocessing
@@ -162,132 +163,6 @@ def compute_averages_std(input_array):
     return average, stand_dev, peak_to_peak_p, peak_to_peak_m
 
 
-def get_observatory_coordinates(observatory):
-    if observatory == 'north':
-        latitude = ddmmss2deg(28, 45, 42.462)
-        longitude = ddmmss2deg(-17, 53, 26.525)
-        return latitude, longitude
-    elif observatory == 'south':
-        latitude = ddmmss2deg(-24, 40, 24.8448)
-        longitude = ddmmss2deg(-70, 18, 58.4712)
-        return latitude, longitude
-    else:
-        print('Unknown observatory!')
-        latitude = np.float(input('Observatory latitude (in degrees):'))
-        longitude = np.float(input('Observatory longitude (in degrees):'))
-        return latitude, longitude
-
-
-def get_winter_months():
-    return [1, 2, 3, 4, 11, 12]
-
-
-def get_summer_months():
-    return [6, 7, 8, 9, 10]
-
-
-def get_all_months():
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-
-def get_intermediate_months():
-    return [5, 6, 10, 11]
-
-
-def get_epoch(epoch):
-    valid_epochs = {'winter': get_winter_months(),
-                    'summer': get_summer_months(),
-                    'all': get_all_months(),
-                    'intermediate': get_intermediate_months()}
-    return valid_epochs[epoch]
-
-
-def get_south_winter_months():
-    return [5,6,7,8,9,10]
-
-
-def get_south_summer_months():
-    return [1,2,3,4,5,10,11,12]
-
-
-def get_south_epoch(epoch):
-    valid_epochs = {'winter': get_south_winter_months(),
-                    'summer': get_south_summer_months(),
-                    'all': get_all_months()}
-    return valid_epochs[epoch]
-
-
-def select_new_epochs_dataframe_north(df,epoch_text):
-
-    epoch = get_epoch(epoch_text)
-
-    if epoch_text == 'winter':
-        condition = (df.month == epoch[0]) | (df.month == epoch[1]) | (df.month == epoch[2]) | \
-                    (df.month == epoch[3]) | ((df.month == epoch[4]) & (df.day > 15)) | (df.month == epoch[5])
-        new_df = df[condition]
-
-    elif epoch_text == 'summer':
-        condition = ((df.month == epoch[0]) & (df.day > 20)) | (df.month == epoch[1]) | (df.month == epoch[2]) | \
-                    (df.month == epoch[3])
-        new_df = df[condition]
-
-    elif epoch_text == 'intermediate':
-        condition = (df.month == epoch[0]) | ((df.month == epoch[1]) & (df.day <= 20)) | \
-                    ((df.month == epoch[2]) & (df.day > 5)) | ((df.month == epoch[3]) & (df.day <= 15))
-        new_df = df[condition]
-
-    return new_df
-
-def select_new_epochs_dataframe_south(df,epoch_text):
-
-    epoch = get_south_epoch(epoch_text)
-
-    if epoch_text == 'summer':
-        condition = (df.month == epoch[0]) | (df.month == epoch[1]) | (df.month == epoch[2]) | (df.month == epoch[3]) |\
-                    ((df.month == epoch[4]) & (df.day < 15)) | ((df.month == epoch[5]) & (df.day > 15)) | \
-                    (df.month == epoch[6]) | (df.month == epoch[7])
-        new_df = df[condition]
-
-    elif epoch_text == 'winter':
-        condition = ((df.month == epoch[0]) & (df.day > 15)) | (df.month == epoch[1]) | (df.month == epoch[2]) | \
-                    (df.month == epoch[3]) | (df.month == epoch[4]) | ((df.month == epoch[5]) & (df.day < 15))
-        new_df = df[condition]
-
-    return new_df
-
-
-
-def get_closest_gridpoint(lat, lon, gridstep):
-    """
-    :param lat: float
-    :param lon: float
-    :param gridstep: float. Step in which the grid is divided (0.75 degrees for ECMWF data
-    and 1.0 degrees for GDAS data)
-    :return: nearest grid point
-    """
-    step = gridstep  # grid step in degrees
-    lons_grid = np.zeros(int(360 / step) + 1)
-    lats_grid = np.zeros(int(180 / step) + 1)
-
-    # getting the grid points:
-    for i in range(len(lons_grid)):
-        lons_grid[i] = step * i
-    for i in range(len(lats_grid)):
-        lats_grid[i] = -90 + step * i
-
-    print('Latidude of interest is %5.2f deg' % lat)
-    print('Longitude of interest is %5.2f deg' % (lon % 360))
-    nearest_lat = find_nearest(lats_grid, lat)
-    print('nearest latitude is: %4.1f deg' % nearest_lat )
-    nearest_lon = find_nearest(lons_grid, lon % 360)
-    print('nearest longitude is: %5.1f deg' % nearest_lon )
-    return nearest_lat, nearest_lon
-
-
-def find_nearest(a, value):  # Function to find the nearest grid position to a given latitude or longitude
-    return a[np.abs(a-value).argmin()]
-
-
 def get_gribfile_variables(file_name):
     """
     Function that returns all the different variable names in a grib file
@@ -358,6 +233,7 @@ def get_grib_file_data(file_name):
     gc.collect()
     return vn, vsn, datadict
 
+
 def fill_RH_gaps(rhdata):
     """ in case the RH data for Plevels 20 and 50 hPa is not present, this function fills these gaps
         with 0.0"""
@@ -371,67 +247,10 @@ def fill_RH_gaps(rhdata):
             RH = np.insert(RH, (i*23, i*23), 0.0)
     return RH
 
+
 def computedensity(p,T):
     return Ns * p / ps * Ts / T
 
-def select_epoch(file, epoch_text):
-    global months, month
-    print("loading and selecting data")
-    file = open(file)
-    date, year, month, day, hour, mjd, p, T, h, n, U, V, RH = np.loadtxt(file, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                                                        10, 11, 12), unpack=True)
-
-    epoch = get_epoch(epoch_text)
-
-    if epoch_text == 'winter':
-        condition = (month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) | (month == epoch[3])
-        date = date[condition]
-        mjd = mjd[condition]
-        h = h[condition]
-        n = n[condition]
-        p = p[condition]
-        T = T[condition]
-        V = V[condition]
-        U = U[condition]
-        RH = RH[condition]
-
-    elif epoch_text == 'summer':
-        condition = (month == epoch[0]) | (month == epoch[1]) | (month == epoch[2])
-        date = date[condition]
-        mjd = mjd[condition]
-        h = h[condition]
-        n = n[condition]
-        p = p[condition]
-        T = T[condition]
-        V = V[condition]
-        U = U[condition]
-        RH = RH[condition]
-
-    elif epoch_text == 'intermediate':
-        condition = (month == epoch[0]) | (month == epoch[1]) | (month == epoch[2]) | (month == epoch[3]) | \
-                    (month == epoch[4])
-        date = date[condition]
-        mjd = mjd[condition]
-        h = h[condition]
-        n = n[condition]
-        p = p[condition]
-        T = T[condition]
-        V = V[condition]
-        U = U[condition]
-        RH = RH[condition]
-
-    elif epoch_text == 'all':
-        date = date
-        mjd = mjd
-        h = h
-        n = n
-        p = p
-        T = T
-        U = U
-        V = V
-        RH = RH
-
-    return date, year, month, day, hour, mjd, p, h, n, T, U, V, RH
 
 def select_dataframe_epoch(df, epoch_text):
     epoch = get_epoch(epoch_text)
