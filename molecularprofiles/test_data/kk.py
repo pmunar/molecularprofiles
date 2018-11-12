153fe646604e02c0d27feb8604d92817e19a5700
pmunar <pere.munaradrover@gmail.com>
2018-11-12 18:06:53 +0100

    added some function to plot wind roses

--- molecularprofiles/test_data/kk.py
+++ molecularprofiles/test_data/kk.py
@@ -58,7 +58,7 @@ for h in [2000., 6000., 10000., 15000., 20000., 25000.]:
     fig.clf()
 
 epochs = ['winter', 'summer', 'intermediate']
-for h in np.arange(2000., 30500., 500).tolist():
+for h in np.arange(2000., 20000., 500).tolist():
     fig = plt.figure()
     ax = plt.subplot(111, projection='polar')
     ax.set_theta_direction(-1)
@@ -76,6 +76,42 @@ for h in np.arange(2000., 30500., 500).tolist():
     ax.set_title('Wind direction and speed at altitude = %s'%(h))
     ax.legend(frameon=True, fancybox=True, framealpha=0.7)
     ax.grid(True)
-    fig.savefig('polar_plot_wind_h'+str(h)+'.png', dpi=300, bbox_inches='tight')
+    fig.savefig('polar_plot_wind_h'+str(h)+'.pdf', bbox_inches='tight')
 #   fig.show()
     fig.clf()
+
+
+def create_wind_speed_dataframe(df):
+    wd_centre_bins = np.arange(7.5,360, 15)
+    ws_hist = []
+    for d in wd_centre_bins:
+        ws_hist.append(np.histogram(df.wind_speed[(df.wind_direction >= d-7.5) & (df.wind_direction < d + 7.5)],
+                                    bins=[0,5,10,20,30,40,50,100])[0])
+
+    df_winds = pd.DataFrame(columns=['direction', '0-5', '5-10', '10-20', '20-30', '30-40', '40-50', '> 50'])
+    ws_new_list = []
+    for j in range(len(ws_hist[0])):
+        li = []
+        for i in range(len(ws_hist)):
+            li.append(ws_hist[i][j])
+        ws_new_list.append(li)
+
+    for i,j in zip(df_winds.keys()[1:], range(len(ws_new_list))):
+        df_winds[i] = ws_new_list[j]
+    df_winds_normalized = df_winds.div(df_winds.sum(axis=1), axis=0)
+    df_winds_normalized['direction'] = wd_centre_bins
+    return df_winds_normalized
+
+def plot_wind_rose(df, name_tag='my_wind_rose'):
+    data = []
+    counter = 0
+    for col in df.columns:
+        if col != 'direction':
+            data.append(go.Area(t=df['direction'], r=df[col],
+                                marker=dict(color=cl.scales['9']['seq']['YlGnBu'][counter]), name=col+' m/s'))
+            counter+=1
+    print(data)
+
+    fig = go.Figure(data=data, layout=go.Layout(orientation=270., barmode='stack'))
+    pio.write_image(fig, name_tag+'_wind_speed_rose.pdf')
+    plotly.offline.plot(fig)
